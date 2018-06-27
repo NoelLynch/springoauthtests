7b24cdd55e92448cbabce744081b10c93643f1e5
noellynch <noellynch@altviz.co>
2018-06-27 15:32:22 +0100

    starting on cloud

---
+++ cloud/resourceserver1/src/main/java/com/noeltest/resourceserver1/RunMain.java
@@ -0,0 +1,13 @@
+package com.noeltest.resourceserver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunMain.class, args);
+    }
+
+}
